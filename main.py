d3ee5d4b644ac55745e91941fa4ae417b73333a4
loseday0 <loseday0@sina.com>
2026-05-12 16:19:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from email.message import EmailMessage
 # ================= 配置区 =================
 CONFIG = {
     "thresholds": {
-        "subscribe_premium": 0.0,
-        "subscribe_premium_2": 0.0,
+        "subscribe_premium": 0.5,
+        "subscribe_premium_2": 0.8,
         "redeem_premium": -0.8,
         "redeem_premium_2": -1.2,
         "rotate_spread": 0.0,
